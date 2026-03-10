00c62c7736cf83c3beb9a7576422d5374f7dc7b9
Snow91108 <sachinrv19@gmail.com>
2026-03-10 21:54:13 +0530

    chore: stabilize monorepo and prepare for hosting

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
 import { ThemeProvider } from "@/components/ThemeProvider";
+
+export const dynamic = "force-dynamic";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
